83cdbae395e89a5f94c81c126a9c275c9ad6ac50
Brandon Muhlestein <nd4spdoffroad@gmail.com>
2018-11-09 00:39:16 -0700

    Created VS solution, add folders, windows, classes

---
+++ Add_Mod_Del_Invoice_Project/Add_Mod_Del_Invoice_Project/Main/clsMainSQL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Mod_Del_Invoice_Project.Main
+{
+    class clsMainSQL
+    {
+    }
+}
---
+++ Add_Mod_Del_Invoice_Project/Add_Mod_Del_Invoice_Project/Search/wndSearch.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Add_Mod_Del_Invoice_Project.Search
+{
+    /// <summary>
+    /// Interaction logic for wndSearch.xaml
+    /// </summary>
+    public partial class wndSearch : Window
+    {
+        public wndSearch()
+        {
+            InitializeComponent();
+        }
+    }
+}
